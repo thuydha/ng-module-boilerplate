1f288c230b92595fd5be828c43f8af71cd1d4be1
thuydha <dohuynhanhthuy@gmail.com>
2016-11-02 21:10:40 +0700

    get data

--- app/js/traoDoi/Controllers.js
+++ app/js/traoDoi/Controllers.js
@@ -1,11 +1,26 @@
 'use strict';
 angular.module('g3cApp.traoDoi')
-	.controller('traoDoiCtrl',['$scope', function traoDoiCtrl( $scope ){}])
-	.controller('traoDoiChiTietCtrl',
-		['$scope','$routeParams', function traoDoiChiTietCtrl( $scope, $routeParams){
-		
-		console.log("$routeParams:");
-		console.log($routeParams);
-		$scope.userID = $routeParams.userid;
-
+	.controller('traoDoiCtrl',['$scope', '$http', function traoDoiCtrl( $scope, $http ){
+		$http.get('../../data/traodoi/vpCuaToi.json')
+		.then(function mySucces(response) {
+        	$scope.vpCuaToi = response.data.vpCuaToi;
+    	}, 
+    	function myError(response) {
+        	console.log(response);
+   		});
+   		$http.get('../../data/traodoi/vpCuaBan.json')
+   		.then(function mySucces(response){
+   			$scope.vpCuaBan = response.data.vpCuaBan;
+   		},
+   		function myError(response){
+   			console.log(response);
+   		});
 	}]);
+	// .controller('traoDoiChiTietCtrl',
+	// 	['$scope','$routeParams', function traoDoiChiTietCtrl( $scope, $routeParams){
+		
+	// 	console.log("$routeParams:");
+	// 	console.log($routeParams);
+	// 	$scope.userID = $routeParams.userid;
+
+	// }]);
--- app/js/traoDoi/module.js
+++ app/js/traoDoi/module.js
@@ -4,10 +4,10 @@ angular.module('g3cApp.traoDoi',[])
 	.config(['$routeProvider', function config($routeProvider){
 		$routeProvider.when('/traoDoi',{
 			controller:'traoDoiCtrl',
-			template: '<h3>day la trang trao doi</h3>'
-		}).
-		when('/traoDoi/:userid',{
-			controller:'traoDoiChiTietCtrl',
-			template: '<p>trao doi chi tiet {{userID}} </p>'
+			templateUrl: 'partial/traoDoi.html'
 		});
+		// when('/traoDoi/:userid',{
+		// 	controller:'traoDoiChiTietCtrl',
+		// 	template: '<p>trao doi chi tiet {{userID}} </p>'
+		// });
 	}]);
